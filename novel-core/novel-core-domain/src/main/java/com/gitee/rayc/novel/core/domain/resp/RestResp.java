78a32177e9178e2c7025fa9243c6dd50349376b9
Ray-C <1259864733@qq.com>
2025-03-27 16:55:20 +0800

    feat: 新增后端统一响应数据格式模板

---
+++ novel-core/novel-core-domain/src/main/java/com/gitee/rayc/novel/core/domain/resp/RestResp.java
@@ -0,0 +1,82 @@
+package com.gitee.rayc.novel.core.domain.resp;
+
+import com.gitee.rayc.novel.core.domain.enums.RespCodeEnum;
+import lombok.Getter;
+
+import java.util.Objects;
+
+/**
+ * @Author: Ray-C
+ * @CreateTime: 2025-03-27
+ * @Description: 后端统一响应数据格式模板
+ * @Version: 1.0
+ */
+@Getter
+public class RestResp<T> {
+
+    /**
+     * 响应码
+     */
+    private final String code;
+
+    /**
+     * 响应消息
+     */
+    private final String message;
+
+    /**
+     * 响应数据
+     */
+    private T data;
+
+    private RestResp() {
+        this.code = RespCodeEnum.OK.getCode();
+        this.message = RespCodeEnum.OK.getMessage();
+    }
+
+    private RestResp(RespCodeEnum errorCode) {
+        this.code = errorCode.getCode();
+        this.message = errorCode.getMessage();
+    }
+
+    private RestResp(T data) {
+        this();
+        this.data = data;
+    }
+
+    /**
+     * 业务处理成功,无数据返回
+     */
+    public static RestResp<Void> ok() {
+        return new RestResp<>();
+    }
+
+    /**
+     * 业务处理成功，有数据返回
+     */
+    public static <T> RestResp<T> ok(T data) {
+        return new RestResp<>(data);
+    }
+
+    /**
+     * 业务处理失败
+     */
+    public static RestResp<Void> fail(RespCodeEnum errorCode) {
+        return new RestResp<>(errorCode);
+    }
+
+    /**
+     * 系统错误
+     */
+    public static RestResp<Void> error() {
+        return new RestResp<>(RespCodeEnum.SYSTEM_ERROR);
+    }
+
+    /**
+     * 判断是否成功
+     */
+    public boolean isOk() {
+        return Objects.equals(this.code, RespCodeEnum.OK.getCode());
+    }
+
+}
