506545be20888df6adaa9f26ba50b0e2819b0e4c
Ray-C <1259864733@qq.com>
2025-03-28 19:18:31 +0800

    feat: 添加全局异常处理器

---
+++ novel-core/novel-core-domain/src/main/java/com/gitee/rayc/novel/core/domain/exception/GlobalExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.gitee.rayc.novel.core.domain.exception;
+
+import com.gitee.rayc.novel.core.domain.enums.RespCodeEnum;
+import com.gitee.rayc.novel.core.domain.resp.RestResp;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @Author: Ray-C
+ * @CreateTime: 2025-03-28
+ * @Description: 全局异常处理器
+ * @Version: 1.0
+ */
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 处理数据校验异常
+     */
+    @ExceptionHandler(BindException.class)
+    public RestResp<Void> handlerBindException(BindException e) {
+        log.error(e.getMessage(), e);
+        return RestResp.fail(RespCodeEnum.USER_REQUEST_PARAM_ERROR);
+    }
+
+    /**
+     * 处理业务异常
+     */
+    @ExceptionHandler(BusinessException.class)
+    public RestResp<Void> handlerBusinessException(BusinessException e) {
+        log.error(e.getMessage(), e);
+        return RestResp.fail(e.getRespCodeEnum());
+    }
+
+    /**
+     * 处理系统异常
+     */
+    @ExceptionHandler(Exception.class)
+    public RestResp<Void> handlerException(Exception e) {
+        log.error(e.getMessage(), e);
+        return RestResp.error();
+    }
+
+}
