d0501af173f185c678302a052fccf108d425612b
Ray-C <1259864733@qq.com>
2025-03-29 16:40:20 +0800

    feat: 新增网关启动类

---
+++ novel-gateway/src/main/java/com/gitee/rayc/novel/gateway/NovelGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.gitee.rayc.novel.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Ray-C
+ * @CreateTime: 2025-03-29
+ * @Description: 网关启动类
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class NovelGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovelGatewayApplication.class);
+    }
+
+}
