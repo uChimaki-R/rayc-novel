3445e6d5b52599cb1bff375b4ef088db49acb355
Ray-C <1259864733@qq.com>
2025-03-30 21:39:12 +0800

    feat: 新增Mybatis-Plus配置类

---
+++ novel-core/novel-core-data/src/main/java/com/gitee/rayc/novel/core/data/config/MybatisPlusConfig.java
@@ -0,0 +1,28 @@
+package com.gitee.rayc.novel.core.data.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: Ray-C
+ * @CreateTime: 2025-03-29
+ * @Description: Mybatis-Plus 配置类
+ * @Version: 1.0
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
