54e1ef3d5c54526e17e6dcc31940b95ea964afa6
Ray-C <1259864733@qq.com>
2025-04-01 14:57:47 +0800

    feat: 新增用户mvc配置，注册用户信息解析拦截器

---
+++ novel-user/src/main/java/com/gitee/rayc/novel/user/config/WebConfig.java
@@ -0,0 +1,36 @@
+package com.gitee.rayc.novel.user.config;
+
+import com.gitee.rayc.novel.core.domain.constant.ApiRouterConsts;
+import com.gitee.rayc.novel.user.interceptor.AuthInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Author: Ray-C
+ * @CreateTime: 2025-03-29
+ * @Description: 用户信息解析拦截器 mvc配置
+ * @Version: 1.0
+ */
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        // 权限认证拦截
+        registry.addInterceptor(authInterceptor)
+            // 拦截会员中心相关请求接口
+            .addPathPatterns(ApiRouterConsts.API_FRONT_USER_URL_PREFIX + "/**")
+            // 放行登录注册相关请求接口
+            .excludePathPatterns(ApiRouterConsts.API_FRONT_USER_URL_PREFIX + "/register",
+                ApiRouterConsts.API_FRONT_USER_URL_PREFIX + "/login")
+            .order(2);
+
+    }
+
+}
