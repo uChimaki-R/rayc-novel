8963abdb71501eb82011618f4e72feaad26a9479
Ray-C <1259864733@qq.com>
2025-04-02 16:04:47 +0800

    feat: 新增作家模块启动类

---
+++ novel-author/src/main/java/com/gitee/rayc/novel/author/NovelAuthorApplication.java
@@ -0,0 +1,25 @@
+package com.gitee.rayc.novel.author;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+/**
+ * @Author: Ray-C
+ * @CreateTime: 2025-03-29
+ * @Description: 作家模块 启动类
+ * @Version: 1.0
+ */
+@SpringBootApplication(scanBasePackages = {"com.gitee.rayc.novel"})
+@MapperScan("com.gitee.rayc.novel.author.mapper")
+@EnableCaching
+@EnableDubbo
+public class NovelAuthorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovelAuthorApplication.class, args);
+    }
+
+}
