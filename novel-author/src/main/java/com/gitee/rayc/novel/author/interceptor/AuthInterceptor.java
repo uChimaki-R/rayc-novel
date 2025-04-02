939eeed88b1b07581c062c832be47ad92c152eff
Ray-C <1259864733@qq.com>
2025-04-02 16:01:15 +0800

    feat: 新增作家信息解析拦截器，解析请求token，获取用户id，查询作家信息并保存到线程上下文中

---
+++ novel-author/src/main/java/com/gitee/rayc/novel/author/interceptor/AuthInterceptor.java
@@ -0,0 +1,88 @@
+package com.gitee.rayc.novel.author.interceptor;
+
+import com.gitee.rayc.novel.author.cache.AuthorInfoCacheManager;
+import com.gitee.rayc.novel.core.api.dto.entity.author.AuthorInfoDto;
+import com.gitee.rayc.novel.core.common.context.AuthContext;
+import com.gitee.rayc.novel.core.common.utils.JwtUtils;
+import com.gitee.rayc.novel.core.domain.constant.SystemConfigConsts;
+import com.gitee.rayc.novel.core.domain.enums.RespCodeEnum;
+import com.gitee.rayc.novel.core.domain.exception.BusinessException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
+
+/**
+ * @Author: Ray-C
+ * @CreateTime: 2025-03-29
+ * @Description: 作家信息 解析拦截器
+ * @Version: 1.0
+ */
+@Component
+@RequiredArgsConstructor
+public class AuthInterceptor implements HandlerInterceptor {
+
+    private final AuthorInfoCacheManager authorInfoCacheManager;
+
+    /**
+     * handle 执行前调用
+     */
+    @SuppressWarnings("NullableProblems")
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+                             Object handler) throws Exception {
+        // 获取登录 JWT
+        String token = request.getHeader(SystemConfigConsts.HTTP_AUTH_HEADER_NAME);
+
+        // 开始认证
+        if (!StringUtils.hasText(token)) {
+            // token 为空
+            throw new BusinessException(RespCodeEnum.USER_LOGIN_EXPIRED);
+        }
+        Long userId = JwtUtils.parseToken(token, SystemConfigConsts.NOVEL_FRONT_KEY);
+        if (Objects.isNull(userId)) {
+            // token 解析失败
+            throw new BusinessException(RespCodeEnum.USER_LOGIN_EXPIRED);
+        }
+        // 作家权限认证
+        AuthorInfoDto authorInfo = authorInfoCacheManager.getAuthor(userId);
+        if (Objects.isNull(authorInfo)) {
+            // 作家账号不存在，无权访问作家专区
+            throw new BusinessException(RespCodeEnum.USER_UN_AUTH);
+        }
+
+        // 设置作家ID到当前线程
+        AuthContext.setAuthorId(authorInfo.getId());
+        // 设置 userId 到当前线程
+        AuthContext.setUserId(userId);
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    /**
+     * handler 执行后调用，出现异常不调用
+     */
+    @SuppressWarnings("NullableProblems")
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+        ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    /**
+     * DispatcherServlet 完全处理完请求后调用，出现异常照常调用
+     */
+    @SuppressWarnings("NullableProblems")
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+        throws Exception {
+        // 清理当前线程保存的用户数据
+        AuthContext.clear();
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+
+}
