7f9f42ffabe44eb30db21baab8ceffc0cbd2b7f7
Ray-C <1259864733@qq.com>
2025-04-03 14:33:48 +0800

    feat: 新增首页实体类相关mapper接口

---
+++ novel-home/src/main/java/com/gitee/rayc/novel/home/mapper/HomeBookMapper.java
@@ -0,0 +1,14 @@
+package com.gitee.rayc.novel.home.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gitee.rayc.novel.home.entity.HomeBook;
+
+/**
+ * @Author: Ray-C
+ * @CreateTime: 2025-03-30
+ * @Description: 小说推荐 Mapper 接口
+ * @Version: 1.0
+ */
+public interface HomeBookMapper extends BaseMapper<HomeBook> {
+
+}
---
+++ novel-home/src/main/java/com/gitee/rayc/novel/home/mapper/HomeFriendLinkMapper.java
@@ -0,0 +1,14 @@
+package com.gitee.rayc.novel.home.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gitee.rayc.novel.home.entity.HomeFriendLink;
+
+/**
+ * @Author: Ray-C
+ * @CreateTime: 2025-03-30
+ * @Description: 友情链接 Mapper 接口
+ * @Version: 1.0
+ */
+public interface HomeFriendLinkMapper extends BaseMapper<HomeFriendLink> {
+
+}
